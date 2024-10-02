2a1977d3fa9224a85b356fc92abc49e5159b40a8
khalilBout <bouternikh.khalil@gmal.com>
2024-10-02 15:16:35 +0100

    up

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -4,24 +4,22 @@ import { FaXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 
 import creditCards from "../../assets/website/credit-cards.webp";
-import {motion} from "framer-motion"
-
+import { motion } from "framer-motion";
 
 const Footer = () => {
-  return <div className="bg-gradient-to-r from-primary to-primaryDark pt-12 pb-8 text-white">
-    <div className=" container">
-<div className="grid grid-col-1 sm:grid-col-2 md:grid-cols-3 gap-8">
-{/* company details section  */}
+  return (
+    <div className="bg-gradient-to-r from-primary to-primaryDark pt-12 pb-8 text-white">
+      <div className=" container">
+        <div className="grid grid-col-1 sm:grid-col-2 md:grid-cols-3 gap-8">
+          {/* company details section  */}
 
-{/* footer link section  */}
-
-{/* social link section  */}
-
-</div>
+          {/* footer link section  */}
+          <h1>Footer</h1>
+          {/* social link section  */}
+        </div>
+      </div>
     </div>
-
-
-  </div>;
+  );
 };
 
 export default Footer;
